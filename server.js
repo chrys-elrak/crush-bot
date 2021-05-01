63a834d74bfcb884bd09416231354fb928d59282
Chrys Rakotonimanana <chrysrakk@gmail.com>
2021-05-01 11:53:26 +0300

    make response

--- server.js
+++ server.js
@@ -13,51 +13,121 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app
-    .get('/privacy-policy', (req, res) => res.sendFile(path.resolve('./terms_and_privacy/privacy-policy.html')))
+  .get('/privacy-policy', (req, res) => res.sendFile(path.resolve('./terms_and_privacy/privacy-policy.html')))
 
-    .get('/terms', (req, res) => res.sendFile(path.resolve('./terms_and_privacy/terms.html')))
+  .get('/terms', (req, res) => res.sendFile(path.resolve('./terms_and_privacy/terms.html')))
 
-    .get('/', (req, res, next) => res.send("HELLO WORLD !"))
+  .get('/', (req, res, next) => res.send("HELLO WORLD !"))
 
-    .get('/webhook', (req, res) => {
-        // Parse the query params
-        let mode = req.query['hub.mode'];
-        let token = req.query['hub.verify_token'];
-        let challenge = req.query['hub.challenge'];
+  .get('/webhook', (req, res) => {
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
 
-        // Checks if a token and mode is in the query string of the request
-        if (mode && token && (mode === 'subscribe' && token === VERIFY_TOKEN)) {
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            return res.status(200).send(challenge);
+    if (mode && token && (mode === 'subscribe' && token === VERIFY_TOKEN)) {
+      console.log('WEBHOOK_VERIFIED');
+      return res.status(200).send(challenge);
+    }
+    return res.sendStatus(403);
+  })
+
+  .post('/webhook', (req, res) => {
+    const body = req.body;
+
+    if (body.object === 'page') {
+
+      body.entry.forEach(function (entry) {
+        const webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+        if (webhook_event.message) {
+          handleMessage(sender_psid, webhook_event.message);
+        } else if (webhook_event.postback) {
+          handlePostback(sender_psid, webhook_event.postback);
         }
-        return res.sendStatus(403);
-    })
+      });
 
-    .post('/webhook', (req, res) => {
-        const body = req.body;
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      res.sendStatus(404);
+    }
+  });
 
-        // Checks this is an event from a page subscription
-        if (body.object === 'page') {
+function handleMessage(sender_psid, received_message) {
 
-            // Iterates over each entry - there may be multiple if batched
-            body.entry.forEach(function (entry) {
+  let response;
 
-                // Gets the message. entry.messaging is an array, but 
-                // will only ever contain one message, so we get index 0
-                const webhook_event = entry.messaging[0];
-                console.log(webhook_event);
-            });
+  if (received_message.text) {
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }
 
-            // Returns a '200 OK' response to all requests
-            res.status(200).send('EVENT_RECEIVED');
-        } else {
-            // Returns a '404 Not Found' if event is not from a page subscription
-            res.sendStatus(404);
+  callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+}
+
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  if (received_message.text) {
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+  } else if (received_message.attachments) {
+
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
         }
-    });
+      }
+    }
+  }
+  callSendAPI(sender_psid, response);
+}
 
 app.listen(PORT, (err) => {
-    if (err) throw err;
-    console.info(`Server listen on port ${PORT}`);
+  if (err) throw err;
+  console.info(`Server listen on port ${PORT}`);
 });
