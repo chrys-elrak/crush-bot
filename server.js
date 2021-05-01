73dae186a3590ed56f8280ad17f0e588571b2dde
Chrys Rakotonimanana <chrysrakk@gmail.com>
2021-05-01 10:00:52 +0300

    init

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const app = express();
+
+dotenv.config();
+
+cors({origin: true});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('port', process.env.port || 3000);
+
+app.get('/', (req, res, next) =>{
+    res.send('<h1>Hello world from Crush bit<h1>');
+});
+
+app.get('/webhook', (req, res) => {
+
+});
+
+app.post('/webhook', (req, res) => {
+
+});
+
+app.listen(app.get('port'), server =>{
+    console.info(`Server listen on port ${app.get('port')}`);
+})
