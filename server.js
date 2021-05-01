7a5a958ea0c3118111b7e600b0991f9a8dafccdb
Chrys Rakotonimanana <chrysrakk@gmail.com>
2021-05-01 11:57:31 +0300

    post back

--- server.js
+++ server.js
@@ -33,37 +33,37 @@ app
 
   .post('/webhook', (req, res) => {
     const body = req.body;
-
     if (body.object === 'page') {
-
       body.entry.forEach(function (entry) {
         const webhook_event = entry.messaging[0];
-        console.log(webhook_event);
         if (webhook_event.message) {
           handleMessage(sender_psid, webhook_event.message);
         } else if (webhook_event.postback) {
           handlePostback(sender_psid, webhook_event.postback);
         }
       });
-
       res.status(200).send('EVENT_RECEIVED');
     } else {
       res.sendStatus(404);
     }
   });
 
-function handleMessage(sender_psid, received_message) {
-
+function handlePostback(sender_psid, received_postback) {
   let response;
 
-  if (received_message.text) {
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
-    }
-  }
+  // Get the payload for the postback
+  let payload = received_postback.payload;
 
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
+  }
+  // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
 }
+
 
 function callSendAPI(sender_psid, response) {
   let request_body = {
