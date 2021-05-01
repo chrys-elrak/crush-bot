056ad560f0d8a1b92f7b013bea779152c3d249a5
Chrys Rakotonimanana <chrysrakk@gmail.com>
2021-05-01 12:32:21 +0300

    handle event

--- server.js
+++ server.js
@@ -37,96 +37,59 @@ app
     if (body.object === 'page') {
       body.entry.forEach(function (entry) {
         const webhookEvent = entry.messaging[0];
-        const senderPsid = webhookEvent.sender.id;
-        console.log('SENDER ID: ' + senderPsid);
-        if (webhookEvent.message) {
-          console.log("MESSAGE");
-          handleMessage(senderPsid, webhookEvent.message);
-        } else if (webhookEvent.postback) {
-          console.log("POST BACK");
-          handlePostback(senderPsid, webhookEvent.postback);
-        }
-        console.log("IDK");
+        const userId = webhookEvent.sender.id;
+        console.log('SENDER ID: ' + userId);
+        handleEvent(webhookEvent);
       });
       return res.status(200).send('EVENT_RECEIVED');
     }
     return res.sendStatus(404);
   });
 
-function handlePostback(senderPsid, received_postback) {
-  let response;
-
-  let payload = received_postback.payload;
-  if (payload === 'yes') {
-    response = { "text": "Thanks!" }
-  } else if (payload === 'no') {
-    response = { "text": "Oops, try sending another image." }
-  }
-  callSendAPI(senderPsid, response);
-}
-
-
-function callSendAPI(senderPsid, response) {
-  let request_body = {
-    "recipient": {
-      "id": senderPsid
-    },
-    "message": response
-  }
-
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
+  function callSendAPI(userId, response) {
+    let request_body = {
+      "recipient": {
+        "id": userId
+      },
+      "message": response
     }
-  });
-}
-
-function handleMessage(senderPsid, receivedMessage) {
-
-  let response;
-
-  if (receivedMessage.text) {
-    response = {
-      "text": `You sent the message: "${receivedMessage.text}". Now send me an attachment!`
-    }
-  } else if (receivedMessage.attachments) {
-
-    let attachment_url = receivedMessage.attachments[0].payload.url;
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
-            ],
-          }]
-        }
+  
+    request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+      if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
       }
+    });
+  }
+  
+
+  function handleEvent(event, userId) {
+
+    console.log(event);
+    
+    if (event.message) {
+      const {text, attachments} = event.message;
+      callSendAPI(userId, response);
+      return;
+    }
+    
+    if (event.postback) {
+      const {payload} = event.postback;
+      if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+      } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+      }
+      callSendAPI(userId, response);
+      return;
     }
   }
-  callSendAPI(senderPsid, response);
-}
 
 app.listen(PORT, (err) => {
   if (err) throw err;
